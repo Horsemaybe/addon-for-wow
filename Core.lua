d2f01b9a34071598f46c7178a0c83cb1dc74c489
Dani-Ray <daniel_raykov@abv.bg>
2017-05-14 12:08:00 +0200

    update

--- Core.lua
+++ Core.lua
@@ -68,3 +68,53 @@ function MythicPlusTimer:OnInitialize()
     
     MythicPlusTimerCMTimer:Init();
 end
+
+function MythicPlusTimer:OnEnable()
+    self:RegisterEvent("CHALLENGE_MODE_START");
+    self:RegisterEvent("CHALLENGE_MODE_COMPLETED");
+    self:RegisterEvent("CHALLENGE_MODE_RESET");
+    self:RegisterEvent("PLAYER_ENTERING_WORLD");
+    
+    self:RegisterChatCommand("mpt", "CMTimerChatCommand");
+end
+
+function MythicPlusTimer:CHALLENGE_MODE_START()
+    MythicPlusTimerCMTimer:OnStart();
+end
+
+function MythicPlusTimer:StartCMTimer()
+    MythicPlusTimer:CancelCMTimer()
+    MythicPlusTimer.cmTimer = self:ScheduleRepeatingTimer("OnCMTimerTick", 1)
+end
+
+function MythicPlusTimer:CHALLENGE_MODE_COMPLETED()
+    MythicPlusTimerCMTimer:OnComplete();
+end
+
+function MythicPlusTimer:CHALLENGE_MODE_RESET()
+    MythicPlusTimerCMTimer:OnReset();
+end
+
+
+function MythicPlusTimer:OnCMTimerTick()
+    MythicPlusTimerCMTimer:Draw();
+end
+
+function MythicPlusTimer:PLAYER_ENTERING_WORLD()
+    MythicPlusTimerCMTimer:ReStart();
+end
+
+function MythicPlusTimer:CancelCMTimer()
+    if MythicPlusTimer.cmTimer then
+        self:CancelTimer(MythicPlusTimer.cmTimer)
+        MythicPlusTimer.cmTimer = nil
+    end
+end
+
+function MythicPlusTimer:CMTimerChatCommand(input)
+    if input == "toggle" then
+        MythicPlusTimerCMTimer:ToggleFrame()
+    else
+        self:Print("/mpt toggle: " .. MythicPlusTimer.L["ToggleCommandText"])
+    end
+end
